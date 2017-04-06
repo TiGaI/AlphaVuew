5259f5c209d545ce0c28d8afbda00257bc102f52
Junjie <junjiejiang94@gmail.com>
2017-04-06 11:58:51 -0700

    test

--- env.sh
+++
@@ -1,5 +0,0 @@
-export AWS_ACCESS_KEY_ID="AKIAJSQRWL7PN6PSU2UQ"
-export AWS_SECRET_ACCESS_KEY="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"
-export AWS_DEFAULT_REGION="us-west-2"
-
-export MONGODB_URI='mongodb://newvuew:newvuew123!@ds127730.mlab.com:27730/newvuew'
