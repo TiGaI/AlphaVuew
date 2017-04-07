22d9efbfd61057cd7b4b36d45f6a1831802041ae
Eric Abenojar <abenoeric@gmail.com>
2017-04-07 10:24:58 -0700

    added createPin component

--- App/components/createPin.js
+++ App/components/createPin.js
@@ -12,14 +12,7 @@ import * as actionCreators from '../actions/initialAction';
 import * as loginAction from '../actions/loginAction';
 import MapView from 'react-native-maps';
 
-//Import navigation components
-import CreatePin from './createPin'
 
-var image5 = {uri: 'https://www.thisiscolossal.com/wp-content/uploads/2016/03/finger-4.jpg'}
-var image4 = {uri: 'https://cdn.playbuzz.com/cdn/b19cddd2-1b79-4679-b6d3-1bf8d7235b89/93794aec-3f17-47a4-8801-a2716a9c4598_560_420.jpg'}
-var image3 = {uri: 'https://iso.500px.com/wp-content/uploads/2016/04/STROHL__ST_1204-Edit-1500x1000.jpg'}
-var image2 = {uri: 'https://static.pexels.com/photos/2855/landscape-mountains-nature-lake.jpg'}
-var image1 = {uri: 'https://upload.wikimedia.org/wikipedia/commons/3/38/Two_dancers.jpg'}
 
 
 var { width, height } = Dimensions.get('window');
@@ -31,7 +24,7 @@ const LONGITUDE = 1;
 const LATITUDE_DELTA = 0.009;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
-class MainPage extends Component {
+class CreatePin extends Component {
 
   constructor(props){
     super(props);
@@ -83,13 +76,7 @@ class MainPage extends Component {
   componentWillUnmount (){
   navigator.geolocation.clearWatch(this.watchID);
 }
-  category(){
-    this.props.navigator.push({
-      component: Categories,
-      backButtonTitle: 'Main'
-    })
-  }
-  createPin(){
+createPin(){
     this.props.navigator.push({
       component: CreatePin,
       backButtonTitle: 'MainPage'
@@ -116,20 +103,11 @@ class MainPage extends Component {
          longitude: this.state.currentPosition.longitude}}
          title='Title'
       />
-      <View style={{flex: 0, alignItems: 'center'}}>
-      <TouchableOpacity onPress={this.category.bind(this)}>
-      <Text
-      style={{borderColor: 'white', borderWidth: 1, marginTop: 150, backgroundColor: 'white', width: 275, padding: 15, color: 'grey', textAlign: 'center', fontSize: 18}}
-      placeholder= 'Select a category'
-      >Find things to do... {this.state.lastPosition}</Text>
-      </TouchableOpacity>
-      </View>
-      <View style={{flex: 1, justifyContent: 'flex-end', alignItems: 'center'}}>
-        <Text style={{fontSize: 12, backgroundColor: 'transparent', fontWeight: '500' }}>Add Location</Text>
-        <View style={{flex: 0, marginBottom: 60, backgroundColor: '#00A8BE', width: 50, height: 50,
-        alignItems: 'center', justifyContent: 'center', borderRadius: 25}}>
+      <View style={{flex: 1, justifyContent: 'flex-end'}}>
+        <View style={{flex: 0, marginBottom: 60, backgroundColor: 'black', width: null, height: null,
+        alignItems: 'center', justifyContent: 'center', borderRadius: 15, opacity: 0.9, padding: 15}}>
           <TouchableOpacity onPress={this.createPin.bind(this)}>
-            <Icon style={{fontSize: 35, color: 'white'}} name='md-add'/>
+            <Text style={{color: 'white', fontWeight: '500'}}>Confirm Pin Location</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -140,60 +118,6 @@ class MainPage extends Component {
     )
   }
 }
-var sports = [{name: 'Baseball'}, {name: 'Basketball'},{name: 'Beach Volleyball'},{name: 'Hiking'},{name: 'Running'},{name: 'Soccer'},{name: 'Tennis'}];
-class Categories extends Component {
-  constructor(props){
-    super(props);
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    this.state = {
-      dataSource: ds.cloneWithRows(sports),
-
-    }
-  }
-  render(){
-    return (
-      <View style={{flex: 1}}>
-      <List>
-      <ListView
-        dataSource={this.state.dataSource}
-        renderRow={(rowData) =>
-                <TouchableOpacity>
-                <ListItem>
-                    <Text>{rowData.name}</Text>
-                </ListItem>
-                </TouchableOpacity>
-        }
-      />
-      </List>
-
-      </View>
-    )
-
-
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-    margin: 10,
-    backgroundColor: 'transparent',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-
 function mapStateToProps(state) {
     return {
         login: state.get('login'),
@@ -209,4 +133,4 @@ function mapDispatchToProps(dispatch) {
     };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(MainPage);
+export default connect(mapStateToProps, mapDispatchToProps)(CreatePin);
