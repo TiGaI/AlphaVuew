178d17a1f48c5ca75ffceb6853121c57bb74ab65
Eric <abenoeric@gmail.com>
2017-04-09 17:40:00 -0700

    Create detailedPin.js

---
+++ App/components/detailedPin.js
@@ -0,0 +1,54 @@
+import React, { Component, PropTypes } from 'react';
+import { AppRegistry, ScrollView, StyleSheet, Text, View,
+  TextInput, TouchableOpacity, NavigatorIOS, ListView, Dimensions, Alert, AsyncStorage, Image } from 'react-native';
+import { Item, Input, Tab, Tabs,Spinner, List, ListItem, Left, Body } from 'native-base';
+import Swiper from 'react-native-swiper';
+import randomcolor from 'randomcolor';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as actionCreators from '../actions/initialAction';
+import * as loginAction from '../actions/loginAction';
+import MapView from 'react-native-maps';
+
+//Import navigation components
+import CreatePin from './createPin'
+
+
+class DetailedPin extends Component {
+  constructor(props){
+    super(props);
+    console.log('DETAILED PINS PROPS', this.props)
+  }
+  render(){
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Category: {this.props.marker.activityCategory}</Text>
+      <Text>Title: {this.props.marker.activityTitle}</Text>
+      <Text>Description: {this.props.marker.activityDescription}</Text>
+      <Text>Duration: {this.props.marker.activityDuration} hr(s)</Text>
+      <Text>Start Time: {this.props.marker.activityStartTime}</Text>
+      <Text>Spots Available {this.props.marker.activityCapacity}</Text>
+      </View>
+    )
+  }
+
+}
+
+function mapStateToProps(state) {
+    return {
+        login: state.get('login'),
+        profile: state.get('profile'),
+        activitiesPageState: state.get('activityPageState')
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(actionCreators, dispatch),
+        loginActions: bindActionCreators(loginAction, dispatch)
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DetailedPin);
